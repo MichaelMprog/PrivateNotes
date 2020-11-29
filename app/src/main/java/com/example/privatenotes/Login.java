4ea7dadb2477f138888b6407771c4be16af5b97c
MichaelMprog <michaelmacallister01@gmail.com>
2020-11-29 12:20:18 -0500

    Revert "non-functional encryption rewrite"
    This reverts commit ede3a5234957beb62276d284f9f47a398368e72f.

--- app/src/main/java/com/example/privatenotes/Login.java
+++ app/src/main/java/com/example/privatenotes/Login.java
@@ -3,62 +3,25 @@ package com.example.privatenotes;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.security.crypto.EncryptedSharedPreferences;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.security.crypto.MasterKey;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
     private EditText enterPassword, newPass, oldPass;
     private Button btnChange, btnNext;
 
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String PASSWORD = "password";
+    public static final String PASSWORD = "text";
     private String userPass;
-    private SharedPreferences sharedPreferences;
-
-    private static final String MASTER_KEY_ALIAS = "key";
-    private static final int KEY_SIZE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Encrypted Shared Preferences
-        KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
-                MASTER_KEY_ALIAS,
-                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .setKeySize(KEY_SIZE)
-                .build();
-        MasterKey masterKey = null;
-        try {
-            masterKey = new MasterKey.Builder(Login.this)
-                    .setKeyGenParameterSpec(spec)
-                    .build();
-            sharedPreferences = EncryptedSharedPreferences.create(
-                Login.this,
-                "sharedPrefs",
-                masterKey,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
-        }catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
 
         // UI initialization
         enterPassword = (EditText) findViewById(R.id.enterPassword);
@@ -73,23 +36,17 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 // when clicked, set password to text in newPass text field
                 String oldInput = oldPass.getText().toString();
-                sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-                // sets userPass equal to string in PASSWORD, or if there is no value, make it empty
-                userPass = sharedPreferences.getString(PASSWORD, "default");
+                loadPassword();
                 // if user put nothing in old password, and current password is null, create new password
                 if ( (oldInput == null || oldInput.isEmpty()) && (userPass == null || userPass.isEmpty()) )
                 {
-                    editor.putString(PASSWORD, newPass.getText().toString());
-                    editor.apply();
-                    Toast.makeText(Login.this, "Password saved", Toast.LENGTH_SHORT).show();
+                    savePassword();
                     ((EditText) findViewById(R.id.newPass)).setText("");
                     ((EditText) findViewById(R.id.oldPass)).setText("");
                 }
                 else if (oldInput.compareTo(userPass) == 0)     // if old input is correct
                 {
-                    editor.putString(PASSWORD, newPass.getText().toString());
-                    editor.apply();
-                    Toast.makeText(Login.this, "Password saved", Toast.LENGTH_SHORT).show();
+                    savePassword();
                     ((EditText) findViewById(R.id.newPass)).setText("");
                     ((EditText) findViewById(R.id.oldPass)).setText("");
                 }
@@ -107,9 +64,7 @@ public class Login extends AppCompatActivity {
            public void onClick(View v) {
                // get password from input when login is pressed
                String inputPass = enterPassword.getText().toString();
-               sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-               // sets userPass equal to string in PASSWORD, or if there is no value, make it empty
-               userPass = sharedPreferences.getString(PASSWORD, "default");
+               loadPassword();
                // if user hasn't set a password yet
                if (userPass.compareTo("") == 0)
                {
@@ -132,14 +87,35 @@ public class Login extends AppCompatActivity {
     }
 
     public void savePassword() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         // saves text in newPass EditText field into PASSWORD shared preference
-
+        editor.putString(PASSWORD, newPass.getText().toString());
+        editor.apply();
+        Toast.makeText(this, "Password saved", Toast.LENGTH_SHORT).show();
     }
 
     public void loadPassword() {
-
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        // sets userPass equal to string in PASSWORD, or if there is no value, make it empty
+        userPass = sharedPreferences.getString(PASSWORD, "default");
     }
 
 
+    /*
+    public void savePassword() {
+        SharedPreferences sharedPreferences = new ObscuredSharedPreferences(this,
+                this.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE));
+        sharedPreferences.edit().putString(PASSWORD, newPass.getText().toString()).apply();
+        Toast.makeText(this, "Password saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadPassword() {
+        SharedPreferences sharedPreferences = new ObscuredSharedPreferences(this,
+                this.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE));
+        // sets userPass equal to string in PASSWORD, or if there is no value, make it empty
+        userPass = sharedPreferences.getString(PASSWORD, null);
+    }
+*/
 
 }
